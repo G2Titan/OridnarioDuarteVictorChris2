69177b9a6e2b1f357bc6f5517955097dfb05cbe5
G2Titan <93502813+G2Titan@users.noreply.github.com>
2023-12-19 10:36:54 -0600

    Implementacion clase pajaro

---
+++ Clases/Pajaro.cs
@@ -0,0 +1,77 @@
+﻿using OridnarioDuarteVictorChris.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OridnarioDuarteVictorChris.Interfaces.IMascota;
+
+namespace OridnarioDuarteVictorChris.Clases
+{
+    internal class Pajaro : IMascota
+    {
+        private string _nombre;
+        private int _edad;
+        private string _temperamento;
+        private static int _id = 1;
+        public int id { get; }
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("No puede tener un nombre vacio");
+                }
+
+            }
+        }
+        public int Edad
+        {
+            get
+            {
+                return _edad;
+            }
+            set
+            {
+                _edad = value;
+                if (value >= 9)
+                {
+                    throw new ArgumentException("No puede tener más de 8 años");
+                }
+            }
+        }
+        public string Temperamento
+        {
+            get
+            {
+                return _temperamento;
+            }
+            set
+            {
+                _temperamento = null;
+            }
+        }
+        public IPersona Dueño { get; set; }
+
+        public void CambiarDueño(IPersona newDueño)
+        {
+            Dueño = newDueño;
+            Console.WriteLine($"El dueño de {Nombre} ha sido cambiado a {newDueño.Nombre}");
+        }
+
+        public void HacerRuido()
+        {
+            Console.WriteLine("Pio pio");
+        }
+        public void Aletear()
+        {
+            Console.WriteLine("-Aleteo de miedo imparable");
+        }
+    }
+}
