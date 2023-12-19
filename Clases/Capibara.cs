e474fcbc4d8ab9572400498295178e3b3310aa87
G2Titan <93502813+G2Titan@users.noreply.github.com>
2023-12-19 10:17:54 -0600

    Creacion de clase capibara
    Falta un par de cosas por implementar

---
+++ Clases/Capibara.cs
@@ -0,0 +1,68 @@
+﻿using OridnarioDuarteVictorChris.Interfaces;
+using System;
+using System.Collections.Generic;
+class Capibara : IMascota
+{
+    private string _nombre;
+    private int _edad;
+    private string _temperamento;
+    public int id => throw new NotImplementedException();
+    public string Nombre 
+    {
+        get
+        {
+            return _nombre;
+        }
+        set
+        {
+            _nombre = value;
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("No puede tener un nombre vacio");
+            }
+
+        }
+    }
+    public int Edad 
+    {
+        get
+        {
+            return _edad;
+        }
+        set
+        {
+            _edad = value;
+            if (value >= 12) 
+            {
+                throw new ArgumentException("La capibara no puede tener más de 11 años");
+            }
+        }
+    }
+    public string Temperamento
+    {
+        get
+        {
+            return _temperamento;
+        }
+        set 
+        {
+            _temperamento = value; 
+            if (_temperamento != value) 
+            {
+                throw new ArgumentException("Es de lo más tranquilo, aunque no quieras");
+            }
+        }
+    }
+    public IPersona Dueño { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void CambiarDueño(IPersona newDueño)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void HacerRuido()
+    {
+        Console.WriteLine("cui cui");
+    }
+}
+
