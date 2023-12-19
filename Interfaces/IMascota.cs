f9cdc977f37cc53e75efb9bd8cfdbd433a613ed4
BecerraSoftware <15234616@modelo.edu.mx>
2023-12-19 08:25:48 -0600

    Añadi propiedades a la interface IMascota

--- Interfaces/IMascota.cs
+++ Interfaces/IMascota.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OridnarioDuarteVictorChris.Interfaces;
 
 namespace OridnarioDuarteVictorChris.Interfaces
 {
-    internal interface IMascota
+    public interface IMascota
     {
+        public int id { get; }
+        public string Nombre { get; set; } 
+        public int Edad {  get; set; }
+        public string Temperamento {  get; set; }
+        public IPersona Dueño { get; set; }
+       void HacerRuido();
+        void CambiarDueño(IPersona newDueño);
+
     }
 }
