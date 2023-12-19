c7c67117aaae11b4ae9e7502183da7eb822d957e
G2Titan <93502813+G2Titan@users.noreply.github.com>
2023-12-19 08:53:10 -0600

    Implementacion de clase persona
    Necesito actualizar la clase conforme a las interfaces para poder seguir trabajando en esta clase

---
+++ Clases/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OridnarioDuarteVictorChris.Clases
+{
+    internal class Persona
+    {
+        private string _nombre;
+        private int _id;
+        static int contador = 0;
+        //List<IMascota> MascotasDePersonas;
+        public Persona(string nombre)
+        {
+            _nombre = nombre;
+            id = contador + 1;
+            contador++;
+        }
+        public int id
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+                _id = value;
+            }
+        }
+        public string nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set 
+            {
+                _nombre = value; 
+                if (string.IsNullOrEmpty(value)) 
+                {
+                    throw new ArgumentException("El nombre no puede ser un espacio en blanco o ser vacio");
+                }
+            }
+        }
+        //public MascotasDePersonas();
+    }
+}
